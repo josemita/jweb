1a421ed225c11fb0acfddc6b5be4aec02f21fb6d
josema <josemita.velez@gmail.com>
2014-04-26 15:07:27 +0200

    26/04/2014

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+ <html>
+
+   <head>
+      <meta charset="utf-8"/>   
+      <title>Título de la web</title>
+      <link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+   </head>
+
+   <body>
+   	<header class="contenedor">
+   		Header
+   	</header>
+
+   		<section class="contenedor">
+   			Contenido de la web
+   		</section>
+      
+    <footer class="contenedor">
+    	Footer
+    </footer>
+   </body>
+
+ </html>
